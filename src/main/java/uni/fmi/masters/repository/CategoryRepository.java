b2563076018fb1d3c3d4e236d38df627c38bd3a1
ivangeorgiev97 <georgiev1313@abv.bg>
2020-11-22 20:00:00 +0200

    Add missing annotations for repositories

--- src/main/java/uni/fmi/masters/repository/CategoryRepository.java
+++ src/main/java/uni/fmi/masters/repository/CategoryRepository.java
@@ -1,9 +1,11 @@
 package uni.fmi.masters.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import uni.fmi.masters.bean.CategoryBean;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<CategoryBean, Integer> {
 
 }
--- src/main/java/uni/fmi/masters/repository/MemoryRepository.java
+++ src/main/java/uni/fmi/masters/repository/MemoryRepository.java
@@ -1,9 +1,11 @@
 package uni.fmi.masters.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import uni.fmi.masters.bean.MemoryBean;
 
+@Repository
 public interface MemoryRepository extends JpaRepository<MemoryBean, Integer> {
 
 }
